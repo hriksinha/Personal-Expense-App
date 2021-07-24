08aecfac9c0383e0062b47b0f2f6f828b280f942
Hrik Sinha <dailyhrik@gmail.com>
2021-07-24 12:06:16 +0530

    update : reading from input in html

--- expense.js
+++ expense.js
@@ -1,2 +1,10 @@
-const element = document.querySelector("#btnCounter");
-console.log(element);
+let totalExpense = 0;
+
+function addExpenseTotal() {
+    const inputElement = document.querySelector("#inputAmount");
+    const textAmount = inputElement.value;
+    console.log(textAmount);
+}
+
+const element = document.querySelector("#btnAddExpense");
+element.addEventListener("click", addExpenseTotal);
