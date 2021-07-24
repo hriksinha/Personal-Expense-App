562942d266581ad0a94274c80b4fd8c16ed2537b
Hrik Sinha <dailyhrik@gmail.com>
2021-07-24 21:35:27 +0530

    update : DOM

--- expense.js
+++ expense.js
@@ -1,9 +1,20 @@
+const headingEl = document.querySelector("#headingTotal");
 let totalExpense = 0;
+headingEl.textContent = totalExpense;
+
 
 function addExpenseTotal() {
     const inputElement = document.querySelector("#inputAmount");
     const textAmount = inputElement.value;
     console.log(textAmount);
+
+
+    const expense= parseInt(textAmount, 10);
+    console.log({ expense });
+
+    totalExpense = totalExpense + expense;
+    console.log({ totalExpense });
+    headingEl.textContent = totalExpense;
 }
 
 const element = document.querySelector("#btnAddExpense");
